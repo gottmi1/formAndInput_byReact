23414bd45a879e73eed42cc52a5752cfea74f3ae
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-08-06 03:14:51 +0900

    커스텀 훅을 사용하여 리펙토링 하기 - Name

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import useInput from "../hooks/use-input";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  // const [formIsVaild, setFormIsVaild] = useState(false); 얘를 state로 사용하지 않고 그냥 불리언 값으로 사용한다
+  const {
+    value: enteredName,
+    isValid: enteredNameValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    InputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
 
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
-
-  const enteredNameValid = enteredName.trim() !== "";
-  // name인풋의 입력된 value가 있을 때, true
-  const nameInputIsInValid = !enteredNameValid && enteredNameTouched;
-  // name인풋에 입력된 값이 없으며 네임터치드가 true일 때(해당 인풋이 blur상태가 되었을 때) 입력한 값이 유효하지 않아요를 출력하기 위해 만들었음.
 
   const enteredEmailVaild = enteredEmail.includes("@");
   const emailInputIsInValid = !enteredEmailVaild && enteredEmailTouched;
@@ -26,17 +27,11 @@ const SimpleInput = (props) => {
   }
   // else {formIsValid = false;} state가 아니라 그냥 값으로 사용하고 있기 때문에 없어도 됨
 
-  const nameInputChangeHandler = (e) => {
-    setEnteredName(e.target.value);
-  };
   const emailInputChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
   };
   // input이 받은 target.value를 각값의 상태로 만듬
 
-  const nameBlurHandler = (e) => {
-    setEnteredNameTouched(true);
-  };
   const emailBlurHandler = (e) => {
     setEnteredEmailTouched(true);
   };
@@ -50,9 +45,10 @@ const SimpleInput = (props) => {
     // }
     // 버튼의 disabled를 제어하고 있기 때문에 필요 없다.
 
-    setEnteredName("");
+    // setEnteredName("");
+    // setEnteredNameTouched(false);
+    resetNameInput();
     setEnteredEmail("");
-    setEnteredNameTouched(false);
     setEnteredEmailTouched(false);
     console.log(`name ${enteredName} , email ${enteredEmail}`);
     // setEnteredName(inputRef.current.value);
@@ -64,7 +60,7 @@ const SimpleInput = (props) => {
     // 서브밋 후 입력창이 초기화 되는 것을 원한다면 state를 사용하는 게 더 좋은 선택이다.
   };
 
-  const nameInputClass = nameInputIsInValid
+  const nameInputClass = nameInputHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -81,10 +77,10 @@ const SimpleInput = (props) => {
           id="name"
           minLength={5}
           value={enteredName}
-          onChange={nameInputChangeHandler}
+          onChange={nameChangeHandler}
           onBlur={nameBlurHandler}
         />
-        {nameInputIsInValid && (
+        {nameInputHasError && (
           <p className="error-text">입력한 값이 유효하지 않아요</p>
         )}
       </div>
